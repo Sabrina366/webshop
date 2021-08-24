565e7021060c64b99227e2a78e509dde5bef9261
Sabrina <sabrinafransson@gmail.com>
2021-08-24 21:46:55 +0200

    cross origin enabled

--- src/frontend/src/store.js
+++ src/frontend/src/store.js
@@ -9,14 +9,7 @@ const store = createStore({
      urls:{
          springUrl: 'http://127.0.0.1:8080',
      },
-        products: [
-            {id: 1, title: "knitting", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", price: 250, img: "src/assets/toy.jpg"},
-            {id: 2, title: "knitting", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor sapien, porta cursus eleifend eget, facilisis ac diam. Sed non feugiat ex. Integer mattis risus ac euismod efficitur.", price: 150, img: "src/assets/toy.jpg"},
-            {id: 3, title: "knitting", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor sapien, porta cursus eleifend eget, facilisis ac diam.", price: 20, img: "src/assets/toy.jpg"},
-            {id: 4, title: "knitting", description: "Integer mattis risus ac euismod efficitur. Vivamus sed ornare mauris. Ut at enim vitae elit blandit viverra vitae in nisi. Vestibulum bibendum nisi lectus, eget ullamcorper mi molestie non.", price: 210, img: "src/assets/toy.jpg"},
-            {id: 5, title: "knitting", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor sapien, porta cursus eleifend eget, facilisis ac diam. Sed non feugiat ex. Integer mattis risus ac euismod efficiturNam.", price: 60, img: "src/assets/toy.jpg"},
-            {id: 6, title: "knitting", description: "Integer mattis risus ac euismod efficitur. Suspendisse malesuada aliquam egestas. Sed fringilla, nibh nec suscipit suscipit, urna magna lobortis nisi, quis facilisis dui nisi non sem.", price: 260, img: "src/assets/toy.jpg"}
-        ],
+        products: [],
      cart: [],
     },
     getters: {
--- src/main/java/com/solo/webshop/Controllers/ProductController.java
+++ src/main/java/com/solo/webshop/Controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.solo.webshop.Controllers;
 import com.solo.webshop.Entities.Product;
 import com.solo.webshop.Services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ProductController {
 
